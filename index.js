ccadac290f1c56ec65ed217f290df2a16f5b8dc3
yadavsumit5680-hub <yadavsumit5680@gmail.com>
2026-07-01 13:40:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,5 +121,5 @@ setInterval(() => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`));
+  console.log(`Server running on port ${PORT}`);
 });
